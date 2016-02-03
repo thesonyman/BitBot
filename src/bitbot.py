74e262f90bb81dee312e2f7a2f4bc79808a4c833
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-02-03 23:58:52 +0100

    sample code to request api

--- src/bitbot.py
+++ src/bitbot.py
@@ -1,7 +1,7 @@
 # Module principal du bot, routing only
 
-
-import urllib.request, json, tkinter
+import urllib.request
+import time
 
 base_url = "https://blockchain.info/fr/charts/"
 chart = "market-price"
@@ -9,6 +9,9 @@ timespan = "all"
 
 url = base_url+chart+"?format=json&timespan="+timespan
 
+start = time.time()
+
 response = urllib.request.urlopen(url)
 
 print(response.read().decode("utf-8"))
+print("in "+str(round(time.time()-start, 3))+" s")
