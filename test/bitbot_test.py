1f8a18d921257b9f312a8ee52243becffb81bda9
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-02-03 23:38:26 +0100

    added test dir

---
+++ test/bitbot_test.py
@@ -0,0 +1,24 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import unittest
+
+
+class  Bitbot_TestCase(unittest.TestCase):
+    #def setUp(self):
+    #    self.foo = Bitbot_()
+    #
+
+    #def tearDown(self):
+    #    self.foo.dispose()
+    #    self.foo = None
+
+    def test_bitbot_(self):
+        #assert x != y;
+        #self.assertEqual(x, y, "Msg");
+        self.fail("TODO: Write test")
+
+if __name__ == '__main__':
+    unittest.main()
+
