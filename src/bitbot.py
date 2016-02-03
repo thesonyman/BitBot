58188a8197713ef54b2490246f9d9dd15c4aaa63
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2016-02-03 07:46:12 +0100

    first http request

--- src/bitbot.py
+++ src/bitbot.py
@@ -1,1 +1,11 @@
-print("Do you like python ? :D")
+import urllib.request
+
+base_url = "https://blockchain.info/fr/charts/"
+chart = "market-price"
+timespan = "all"
+
+url = base_url+chart+"?format=json&timespan="+timespan
+
+response = urllib.request.urlopen(url)
+
+print(response.read())
